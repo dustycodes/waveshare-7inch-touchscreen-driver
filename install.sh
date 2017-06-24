c29083076b3ce2d683ae900b77f3039380ce5da2
stixxer <dustymargyle@gmail.com>
2017-06-24 14:10:00 -0600

    Switching to pip3 command instead

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
+# Works with Ubuntu mate 16.04 fresh installation
 # For checking version numbers greater than or equal.
 check_ver_lte() {
     [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
-
 chmod +x *.sh *.py
 sudo patch -b /boot/config.txt 7inch.patch
 sudo apt-get install -y python3-pip libudev-dev
-sudo pip-3.2 install python-uinput pyudev
-#if pip-3.2 can't be found, please use pip3
-#sudo pip3 install python-uinput pyudev
+sudo pip3 install python-uinput pyudev
 
 PYTHON_VERSION=`python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))'`
 if check_ver_lte "3.4" $PYTHON_VERSION; then
